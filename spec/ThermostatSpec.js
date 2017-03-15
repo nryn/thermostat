28006fd777ac06434e1ceb341f678419971be5be
John Chang <johnchang9094@gmail.com>
2017-03-15 16:03:42 +0000

    Added Power saving mode off and robust tests

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,6 +1,7 @@
 describe("Thermostat", function() {
   var thermostat;
   var initial;
+  var powerSave
 
   it("checks to see if it exists", function() {
     expect(Thermostat).not.toBeUndefined();
@@ -34,5 +35,21 @@ describe("Thermostat", function() {
 
   it("cannot change the temperature above 25 if power saving mode is on", function() {
     expect(function() {thermostat._changeTemp(6)}).toThrow();
+  });
+
+  it("cannot change the temperature above 32 if power saving mode is off", function() {
+    thermostat.powerSaver = false;
+    expect(function() {thermostat._changeTemp(6)}).not.toThrow();
+    expect(function() {thermostat._changeTemp(13)}).toThrow();
+  });
+
+  it("knows when the maximum temperature has been reached in powersave mode", function() {
+    expect(thermostat._maxTempReached(6)).toBe(true);
+  });
+
+  it("knows when the maximum temperature has been reached outside powersave mode", function() {
+    thermostat.powerSaver = false;
+    expect(thermostat._maxTempReached(6)).toBe(false);
+    expect(thermostat._maxTempReached(13)).toBe(true);
   });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -14,7 +14,11 @@ Thermostat.prototype.down = function() {
 Thermostat.prototype._changeTemp = function(number) {
   if (this.temp + number < 10)
     throw "minimum temparature reached";
-  if (this.temp + number > 25 && this.powerSaver === true) 
+  if (this._maxTempReached(number))
     throw "maximum temperature reached"
   return this.temp += number;
+};
+
+Thermostat.prototype._maxTempReached = function(number) {
+  return (this.temp + number > 25 && this.powerSaver === true) || (this.temp + number > 32 && this.powerSaver === false);
 };
