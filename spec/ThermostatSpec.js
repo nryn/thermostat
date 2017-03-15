0ce5c31dba9736f210f71b6338c10f9e7d50c3db
John Chang <johnchang9094@gmail.com>
2017-03-15 14:56:18 +0000

    Implemented changeTemp function, and added a mnimimum temperature value to the Thermostat.

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,10 +1,34 @@
 describe("Thermostat", function() {
+  var thermostat;
+  var initial;
+
   it("checks to see if it exists", function() {
     expect(Thermostat).not.toBeUndefined();
   });
 
+  beforeEach(function() {
+    thermostat = new Thermostat();
+    initial = thermostat.temp
+  });
+
+
   it("starts at 20 degrees", function() {
-    var thermostat = new Thermostat();
     expect(thermostat.temp).toEqual(20);
+  });
+
+  it("can increase the temperature", function() {
+    expect(thermostat.up()).toEqual(initial + 1);
+  });
+
+  it("can decrease the temperature", function() {
+    expect(thermostat.down()).toEqual(initial - 1);
+  });
+
+  it("can change the temperature", function() {
+    expect(thermostat._changeTemp(-5)).toEqual(initial - 5)
+  });
+
+  it("cannot change the temperature below the minimum", function() {
+    expect(function() {thermostat._changeTemp(-11)}).toThrow()
   });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,3 +1,17 @@
 function Thermostat() {
   this.temp = 20;
 };
+
+Thermostat.prototype.up = function() {
+  return this._changeTemp(+1);
+};
+
+Thermostat.prototype.down = function() {
+  return this._changeTemp(-1);
+};
+
+Thermostat.prototype._changeTemp = function(number) {
+  if (this.temp + number < 10)
+    throw "minimum temparature reached";
+  return this.temp += number;
+};
